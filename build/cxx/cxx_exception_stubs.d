a21a2ec1626a111bbaa31ff450178474847f66ff
nicolaulod <nicolaulod@gmail.com>
2020-12-09 23:13:33 -0300

    commit 2

--- build/cxx/cxx_exception_stubs.d
+++
@@ -1,6 +0,0 @@
-cxx_exception_stubs.o: \
- /opt/esp-idf/components/cxx/cxx_exception_stubs.cpp \
- /home/nicooo/Code/esp32/apps/weather_station/build/include/sdkconfig.h
-
-/home/nicooo/Code/esp32/apps/weather_station/build/include/sdkconfig.h:
-/opt/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
--- build/esp32-ps3/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(IDF_PATH)/components/esp32-ps3/src/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/esp32-ps3 -lesp32-ps3
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += esp32-ps3
-COMPONENT_LDFRAGMENTS += 
-component-esp32-ps3-build: component-bt-build
